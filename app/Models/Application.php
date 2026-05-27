33ccf10280415b01accd35306d20255867a9957a
Lc0d3r <younesscoder@gmail.com>
2026-05-27 16:33:37 +0100

    feat: add fillable properties to Application model for improved mass assignment

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    //
+    protected $fillable = [
+        'type',
+        'name',
+        'email',
+        'organization',
+        'message'
+    ];
 }
